38bc0408f2dfd16d2389f8b1f804aee63df5a8fe
TGOByrne <tg.obyrne@gmail.com>
2015-03-12 10:39:46 -0600

    Adding missed file

---
+++ OpenTK/Objects/Shapes.cs
@@ -0,0 +1,49 @@
+﻿
+using OpenTK.Graphics;
+
+namespace OpenTK.Objects
+{
+	public static class Shapes
+	{
+
+		public static void DrawSquare(float x, float y, float z, float length)
+		{
+			GL.Begin(BeginMode.Quads);
+
+			GL.Color3(1.0f, 1.0f, 1.0f);
+
+			GL.Vertex3(x - length, y - length, 4.0f + z);
+			GL.Vertex3(x + length, y - length, 4.0f + z);
+			GL.Vertex3(x + length, y + length, 4.0f + z);
+			GL.Vertex3(x - length, y + length, 4.0f + z);
+
+			GL.End();
+		}
+
+		public static void DrawCube(float x, float y, float z, float length)
+		{
+			var l = length/2;
+			GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+			GL.Begin(BeginMode.Triangles);
+
+			GL.Color3(1.0f, 1.0f, 1.0f);
+
+			GL.Vertex3(x - l, y - l, z + 4 - l); GL.Vertex3(x - l, y - l, z + 4 + l); GL.Vertex3(x - l, y + l, z + 4 + l);
+			GL.Vertex3(x + l, y + l, z + 4 - l); GL.Vertex3(x - l, y - l, z + 4 - l); GL.Vertex3(x - l, y + l, z + 4 - l);
+			GL.Vertex3(x + l, y - l, z + 4 + l); GL.Vertex3(x - l, y - l, z + 4 - l); GL.Vertex3(x + l, y - l, z + 4 - l);
+			GL.Vertex3(x + l, y + l, z + 4 - l); GL.Vertex3(x + l, y - l, z + 4 - l); GL.Vertex3(x - l, y - l, z + 4 - l);
+			GL.Vertex3(x - l, y - l, z + 4 - l); GL.Vertex3(x - l, y + l, z + 4 + l); GL.Vertex3(x - l, y + l, z + 4 - l);
+			GL.Vertex3(x + l, y - l, z + 4 + l); GL.Vertex3(x - l, y - l, z + 4 + l); GL.Vertex3(x - l, y - l, z + 4 - l);
+			GL.Vertex3(x - l, y + l, z + 4 + l); GL.Vertex3(x - l, y - l, z + 4 + l); GL.Vertex3(x + l, y - l, z + 4 + l);
+			GL.Vertex3(x + l, y + l, z + 4 + l); GL.Vertex3(x + l, y - l, z + 4 - l); GL.Vertex3(x + l, y + l, z + 4 - l);
+			GL.Vertex3(x + l, y - l, z + 4 - l); GL.Vertex3(x + l, y + l, z + 4 + l); GL.Vertex3(x + l, y - l, z + 4 + l);
+			GL.Vertex3(x + l, y + l, z + 4 + l); GL.Vertex3(x + l, y + l, z + 4 - l); GL.Vertex3(x - l, y + l, z + 4 - l);
+			GL.Vertex3(x + l, y + l, z + 4 + l); GL.Vertex3(x - l, y + l, z + 4 - l); GL.Vertex3(x - l, y + l, z + 4 + l);
+			GL.Vertex3(x + l, y + l, z + 4 + l); GL.Vertex3(x - l, y + l, z + 4 + l); GL.Vertex3(x + l, y - l, z + 4 + l);
+
+			GL.End();
+		}
+
+	}
+}
