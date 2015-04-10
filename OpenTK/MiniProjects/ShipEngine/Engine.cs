4749d0af00e21281cbe02edbafdb3d77e1522687
Tony O'Byrne <tg.obyrne@gmail.com>
2015-04-09 22:47:10 -0600

    Adding some files missed by previous commit

---
+++ OpenTK/MiniProjects/ShipEngine/Engine.cs
@@ -0,0 +1,191 @@
+﻿using System.Drawing;
+using System.Linq;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using OpenTK.Objects;
+using System;
+using System.Collections.Generic;
+
+namespace OpenTK.MiniProjects.ShipEngine
+{
+	public class Engine : GameProject
+	{
+		private Random R { get; set; }
+		public List<Thruster> Engines { get; set; }
+		public List<Thruster> ManeuveringThrusters { get; set; }
+
+		int Angle { get; set; }
+		int ShipTurn { get; set; }
+
+		public Engine()
+		{
+			R = new Random(DateTime.Now.Millisecond);
+
+			ClearColor = Color.Black;
+
+			Angle = 0;
+			ShipTurn = 0;
+		}
+
+		public override void Init()
+		{
+			Engines = new List<Thruster>
+			{
+				new Thruster(new Vector3(0.1f, 0, 0.2f), R).Forward,
+				new Thruster(new Vector3(0.1f, 0, -0.2f), R).Forward,
+				new Thruster(new Vector3(0, -0.05f, 0), R).Forward,
+				new Thruster(new Vector3(0, 0.05f, 0), R).Forward
+			};
+
+			ManeuveringThrusters = new List<Thruster>
+			{
+				new Thruster(new Vector3(-1f, 0, 0.1f), R).Starboard,
+				new Thruster(new Vector3(-1f, 0, -0.1f), R).Port,
+				new Thruster(new Vector3(-1.1f, 0, 0.0f), R).Reverse
+			};
+
+			Initialized = true;
+		}
+
+		public override void OnRenderFrame(FrameEventArgs e, GameWindow gw)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			var modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadMatrix(ref modelview);
+
+			//	Translate to 0,0,0
+
+
+			//	Translate back
+
+			foreach (var engine in Engines)
+			{
+				engine.Draw();
+			}
+
+			foreach (var thruster in ManeuveringThrusters)
+			{
+				thruster.Draw();
+			}
+
+			Shapes.Platform();
+
+			gw.SwapBuffers();
+		}
+
+		public override void OnKeyDown(KeyboardKeyEventArgs keyboardKeyEventArgs, GameWindow game)
+		{
+			switch (keyboardKeyEventArgs.Key)
+			{
+				case Key.W:
+					foreach (var thruster in Engines)
+					{
+						thruster.ThrustOn();
+					}
+					break;
+
+				case Key.S:
+					var reverseThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Reverse);
+					foreach (var thruster in reverseThrusters)
+					{
+						thruster.ThrustOn();
+					}
+					break;
+
+				case Key.A:
+					var portThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Port);
+					foreach (var thruster in portThrusters)
+					{
+						thruster.ThrustOn();
+
+						//	We increase for each thruster since more thrust => more turning
+						ShipTurn++;
+					}
+					if (Angle >= 360)
+					{
+						Angle -= 360;
+					}
+					break;
+
+				case Key.D:
+					var starboardThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Starboard);
+					foreach (var thruster in starboardThrusters)
+					{
+						thruster.ThrustOn();
+
+						//	We increase for each thruster since more thrust => more turning
+						ShipTurn--;
+					}
+					break;
+			}
+
+			Angle += ShipTurn;
+			if (Angle < 0) { Angle += 360; }
+			else if (Angle > 360) { Angle -= 360; }
+		}
+
+		public override void OnKeyUp(KeyboardKeyEventArgs keyboardKeyEventArgs, GameWindow game)
+		{
+			switch (keyboardKeyEventArgs.Key)
+			{
+				case Key.W:
+					foreach (var thruster in Engines)
+					{
+						thruster.ThrustOff();
+					}
+					break;
+
+				case Key.S:
+					var reverseThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Reverse);
+					foreach (var thruster in reverseThrusters)
+					{
+						thruster.ThrustOff();
+					}
+					break;
+
+				case Key.A:
+					var portThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Port);
+					foreach (var thruster in portThrusters)
+					{
+						thruster.ThrustOff();
+					}
+					break;
+
+				case Key.D:
+					var starboardThrusters = ManeuveringThrusters.Where(x => x.Direction == Maneuvering.Thrust.Starboard);
+					foreach (var thruster in starboardThrusters)
+					{
+						thruster.ThrustOff();
+					}
+					break;
+			}
+		}
+	}
+
+	public struct Maneuvering
+	{
+		public enum Thrust
+		{
+			Port,
+			Starboard,
+			Forward,
+			Reverse
+		}
+
+		[Flags]
+		public enum ThrusterPosition
+		{
+			None = 0x0000,
+			Nose = 0x0001,
+			Tail = 0x0002,
+			Bottom = 0x0004,
+			Top = 0x0008,
+			Front = 0x0016,
+			Rear = 0x0032,
+			Left = 0x0064,
+			Right = 0x0128,
+		}
+	}
+}
---
+++ OpenTK/MiniProjects/ShipEngine/Thruster.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using OpenTK.Graphics;
+using OpenTK.MiniProjects.ParticleFountain;
+
+namespace OpenTK.MiniProjects.ShipEngine
+{
+	public class Thruster
+	{
+		public Vector3 Position { get; set; }
+		public List<Particle> Particles { get; set; }
+		public Random R { get; set; }
+
+		public bool FireThrust { get; set; }
+
+		protected Vector3 Orientation { get; set; }
+
+		public Maneuvering.Thrust Direction { get; protected set; }
+
+		public Thruster Port
+		{
+			get
+			{
+				Direction = Maneuvering.Thrust.Port;
+				Orientation = Vector3.UnitZ * -1;
+				return this;
+			}
+		}
+		public Thruster Starboard
+		{
+			get
+			{
+				Direction = Maneuvering.Thrust.Starboard;
+				Orientation = Vector3.UnitZ;
+				return this;
+			}
+		}
+		public Thruster Forward
+		{
+			get
+			{
+				Direction = Maneuvering.Thrust.Forward;
+				Orientation = Vector3.UnitX;
+				return this;
+			}
+		}
+		public Thruster Reverse
+		{
+			get
+			{
+				Direction = Maneuvering.Thrust.Reverse;
+				Orientation = Vector3.UnitX * -1;
+				return this;
+			}
+		}
+
+		public Thruster(Vector3 position, Random r)
+		{
+			Position = position;
+			R = r;
+			Particles = new List<Particle>();
+
+			FireThrust = false;
+		}
+
+		public void Draw()
+		{
+			Particles.RemoveAll(x => x.ParticleExpired);
+
+			if (FireThrust)
+			{
+				Thrust();
+			}
+
+			foreach (var particle in Particles)
+			{
+				particle.Move();
+				particle.Draw();
+			}
+		}
+
+		public void Thrust()
+		{
+			var startX = Position.X;
+			var startY = Position.Y;
+			var startZ = Position.Z;
+
+			Particles.Add(
+				new Particle(startX, startY, startZ, R)
+				{
+					Gravity = false,
+					Orientation = Orientation
+				}
+					.Slow
+				);
+		}
+
+		public void ThrustOn()
+		{
+			FireThrust = true;
+		}
+
+		public void ThrustOff()
+		{
+			FireThrust = false;
+		}
+
+		public void Set(Maneuvering.ThrusterPosition thrusterPosition)
+		{
+			ThrusterPosition = thrusterPosition;
+		}
+
+		public void Set(Vector3 orientation)
+		{
+			Orientation = orientation;
+		}
+
+		public Maneuvering.ThrusterPosition ThrusterPosition { get; protected set; }
+	}
+}
